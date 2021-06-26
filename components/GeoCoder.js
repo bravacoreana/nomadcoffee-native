944f006625786d504a871a95b88adb3b7b01e5f1
Liha Kim <lihakim.kr@gmail.com>
2021-06-26 17:58:44 +0200

    [add] Add political result type to Geocoder

--- components/GeoCoder.js
+++ components/GeoCoder.js
@@ -1,28 +1,29 @@
 import React, { useState } from "react";
-import Geocoder from "react-native-geocoding";
 import { Text } from "react-native";
+import Geocoder from "react-native-geocoding";
 
 export default function Maps({ latitude, longitude }) {
   const [address1, setAddress1] = useState("");
   const [address2, setAddress2] = useState("");
-  const [city, setCity] = useState("");
-  Geocoder.init("AIzaSyA0-2t63DuQXrAY6_aOR72w2RbWWMaU5Ak");
+  Geocoder.init("AIzaSyA0-2t63DuQXrAY6_aOR72w2RbWWMaU5Ak", {
+    result_type: "political",
+  });
   Geocoder.from(latitude, longitude)
     .then((json) => {
-      setAddress1(json.results[0].address_components[2].long_name);
-      setAddress2(json.results[0].address_components[3].long_name);
+      setAddress1(json.results[0].address_components[1].short_name);
+      setAddress2(json.results[0].address_components[2].short_name);
       let components = json.results[0].address_components;
       components.forEach(
         (component) =>
           component.types.includes("administrative_area_level_1") &&
-          setCity(component.long_name)
+          setAddress2(component.long_name)
       );
     })
     .catch((error) => console.warn(error));
 
   return (
     <Text style={{ color: "white" }}>
-      {address1}, {city}
+      {address1}, {address2}
     </Text>
   );
 }
